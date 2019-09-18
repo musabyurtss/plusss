5a9725d89a8ee525cfb7a74974d31ea5b7eaddd7
musabyurtss <musab.yurtss@gmail.com>
2019-09-18 14:07:34 +0300

    1 binaries uploaded

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ import './style.scss';
 
 
 
+const tooltip = document.querySelector('.tooltip');
+
+
+setInterval(() => {
+  let d = new Date();
+  tooltip.innerHTML = `${d.getMinutes() > 9 ? d.getMinutes() : '0' + d.getMinutes()}:${d.getSeconds() > 9 ? d.getSeconds() : '0' + d.getSeconds()}`;
+
+}, 499)
